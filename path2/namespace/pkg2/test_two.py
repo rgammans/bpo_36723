78d3c10bbfe068074048f73b199047aba9419540
Roger Gammans <rgammans@gammascience.co.uk>
2019-08-14 20:22:22 +0100

    Simple test shell script for python.dot.org bug 36723

---
+++ path2/namespace/pkg2/test_two.py
@@ -0,0 +1,6 @@
+
+import unittest
+
+class PackageOne(unittest.TestCase):
+    def test_exists(self,):
+        pass
---
+++ test.sh
@@ -0,0 +1,33 @@
+export PYTHON=python3
+rm -f tmpfifo
+(
+    cd path2/namespace/pkg2/
+    echo "## Should be exactly one passing unittest"
+    $PYTHON -m unittest discover
+)
+
+(
+    cd path1/namespace/pkg1/
+    echo "## Should be exactly one passing unittest"
+    $PYTHON -m unittest discover
+)
+(
+    export PYTHONPATH=path1:path2
+    if $PYTHON -m namespace.pkg1 ; then 
+        echo "PKG1 found"
+    else
+        echo "## Can't traverse namesepace package correctly - test invalid"
+    fi
+    if $PYTHON -m namespace.pkg2 ; then 
+        echo "PKG2 found"
+    else
+        echo "## Can't traverse namesepace package correctly - test invalid"
+    fi
+    
+    echo "## Should be exactly two passing unittests"
+    (
+        $PYTHON -m unittest discover 2>&1 1>&3 | tee output >&2
+    ) 3>&1
+    ##Set return code for failure`
+    grep "Ran 2 tests" output
+)
